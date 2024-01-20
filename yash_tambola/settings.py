2da2e15a73160c6399d57eee31ca0969f099efc6
Yashjain197 <191b291@juetguna.in>
2024-01-20 15:13:29 +0530

    allowed host for aws

--- yash_tambola/settings.py
+++ yash_tambola/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(3v)9mtgyd+-j8$1toor*oa5mqb34o1p*zh-t266oxsg^f3xcj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
